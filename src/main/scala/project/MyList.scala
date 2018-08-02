24671ade17529b783e5ee15b1bef0826ddd639ff
kevin.lambe <kevin.lambe@workday.com>
2018-08-02 16:21:12 +0100

    addedmap, filter and flatmap functions

--- src/main/scala/project/MyList.scala
+++ src/main/scala/project/MyList.scala
@@ -18,6 +18,13 @@ abstract class MyList[+A] {
 
   override def toString: String = "[" + printElements + "]"
 
+  def map[B](myTransformer: MyTransformer[A, B]): MyList[B]
+
+  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B]
+  def filter(predicate: MyPredicate[A]): MyList[A]
+
+  def ++[B>:A](list:MyList[B]):MyList[B]
+
 }
 
 object Empty extends MyList[Nothing] {
@@ -31,6 +38,12 @@ object Empty extends MyList[Nothing] {
   def isEmpty: Boolean = true
 
   def printElements: String = ""
+
+  override def map[B](myTransformer: MyTransformer[Nothing, B]): MyList[Nothing] = Empty
+
+  def flatMap[B](transformer: MyTransformer[Nothing, MyList[B]]): MyList[B] = Empty
+  def filter(predicate: MyPredicate[Nothing]): MyList[Nothing] = Empty
+  def ++[B>:Nothing](list:MyList[B]):MyList[B] = list
 }
 
 
@@ -47,5 +60,19 @@ class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
   def printElements: String =
     if (t.isEmpty) "" + h
     else h + " " + t.printElements
+
+  def map[B](myTransformer: MyTransformer[A, B]): MyList[B] =
+    new Cons(myTransformer.transform(h), t.map(myTransformer))
+
+
+  def filter(predicate: MyPredicate[A]): MyList[A] =
+    if (predicate.test(h)) new Cons(h, t.filter(predicate))
+    else t.filter(predicate)
+
+  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B] =
+    transformer.transform(h) ++ t.flatMap(transformer)
+
+  def ++[B>:A](list:MyList[B]):MyList[B] = new Cons(h, t ++ list)
+
 }
 
